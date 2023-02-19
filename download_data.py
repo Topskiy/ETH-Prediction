74421279a28a1e2ea07ad3ed3cae460d2e0e98fe
pfed-prog <fedorovn19@gmail.com>
2023-02-19 22:46:16 +0100

    added prediction notebook

--- download_data.py
+++ download_data.py
@@ -11,9 +11,9 @@ client = Client(api_key, api_secret)
 SYMBOL = "ETHUSDT"
 
 # 1m 5m 15m 1d
-INTERVAL='1m'
+INTERVAL='1h'
 
-klines = client.get_historical_klines(SYMBOL, INTERVAL, "2022-12-11 20:03:00")
+klines = client.get_historical_klines(SYMBOL, INTERVAL, "2023-02-19 19:00:00")
 data = pd.DataFrame(klines)
  # create colums name
 data.columns = ['open_time','open', 'high', 'low', 'close', 'volume','close_time',
